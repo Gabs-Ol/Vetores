08224b6d3e5073fbae2831e8023da1ef7dba1dbe
Gabriel Oliveira <gabriel.silva3594@etec.sp.gov.br>
2023-11-07 18:01:19 -0300

    Vetores Decrescente e Crescente

--- vetorescrescente.c
+++ vetorescrescente.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+void main() {
 	int vetor[7];
 	int temp;
 
@@ -12,17 +12,17 @@ int main() {
 	
 	for ( int i = 0; i < 7 - 1; i++){
 		for ( int j = 0; j < 7 - i - 1; j++) {
-		       if (vetor[i] > vetor[j + 1]) {
+		       if (vetor[j] < vetor[j - 1]) {
 		       temp = vetor[j];
-	               vetor[j] = vetor[j + 1];
-	               vetor[j + 1] = temp;
+	               vetor[j] = vetor[j - 1];
+	               vetor[j - 1] = temp;
 		       }	       
 	}
 }
+        printf("Valores ordenados em ordem crescente:\n");
         for (int i = 0; i < 7; i++) {
 		printf("%d\n", vetor[i]);
 	}
-       return 0;
        }
 
 
